5f8a2ecbd9034bd79c7e01faf1c87a273723e677
unknown <thanasisprovele@gmail.com>
2023-05-31 14:08:13 +0300

    Add moodle core

---
+++ local/registrer_login_plugin/version.php
@@ -0,0 +1,5 @@
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2023053103;  // YYYYMMDDHH (Year Month Day 24-hr time).
+$plugin->requires = 2022041200; // YYYYMMDDHH (The release version of Moodle 4.1).
+$plugin->component = 'local_plugin'; // Name of your plugin (used for diagnostics).
